07da02aa5e04fe306a4c7a3c544c44a30850ca9e
roboli <robertooliveros.cg@outlook.com>
2024-03-27 16:23:37 -0600

    Fix var names

--- src/lox-function.ts
+++ src/lox-function.ts
@@ -1,23 +1,23 @@
 import { Interpreter, LoxCallable, Fun, Environment } from "./lox-ts";
 
 export class LoxFunction implements LoxCallable {
-  fun: Fun;
+  declaration: Fun;
 
-  constructor(fun: Fun) {
-    this.fun = fun;
+  constructor(declaration: Fun) {
+    this.declaration = declaration;
   }
 
   arity() {
-    return this.fun.params.length;
+    return this.declaration.params.length;
   }
 
   call(interpreter: Interpreter, args: any[]): any {
-    let environment = new Environment(interpreter.environment);
+    let closure = new Environment(interpreter.globals);
 
     for (let i = 0; i < args.length; i++) {
-      environment.define(this.fun.params[i].lexeme, args[i]);
+      closure.define(this.declaration.params[i].lexeme, args[i]);
     }
 
-    interpreter.executeBlock(this.fun.body, environment);
+    interpreter.executeBlock(this.declaration.body, closure);
   }
 }
